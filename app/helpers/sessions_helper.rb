28f355964a435dcdde4f0f20ba3e6fe7c0d9336e
adambajumba <isaacjumba@gmail.com>
2015-11-24 13:32:13 +0000

    Authenticate log in and create log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,17 @@
 module SessionsHelper
+
+	#Logs in the given User
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	#Returns the current logged-in user (idf any)
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	#Returns True if the User is Logged in, false otherwies
+	def loggged_in?
+		!current_user.nil?
+	end
 end
