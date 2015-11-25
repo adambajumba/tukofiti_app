927ae52a85f46bd15185f0cd6989bffc7fc21b8d
adambajumba <isaacjumba@gmail.com>
2015-11-25 12:02:01 +0000

    Change identation : tab width to two spaces

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,51 +1,51 @@
 module SessionsHelper
 
-	#Logs in the given User
-	def log_in(user)
-		session[:user_id] = user.id
-	end
+  #Logs in the given User
+  def log_in(user)
+    session[:user_id] = user.id
+  end
 
-	#Returns the current logged-in user (idf any)
-	def current_user
-		@current_user ||= User.find_by(id: session[:user_id])
-	end
+  #Returns the current logged-in user (idf any)
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-	#Remembers a user in a persistent session
-	def remember(user)
-		user.remember
-		cookies.permanent.signed[:user_id] = user.id
-		cookies.permanent[:remember_token] = user.remember_token
-	end
+  #Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
-	#Returns the user corresponding to the remember token choice
-	def current_user
-		if (user_id = session[:user_id])
-			@current_user ||= User.find_by(id: user_id)
-			elsif (user_id = cookies.signed[:user_id])
-				user = User.find_by(id: user_id)
-				if user && user.authenticated?(cookies[:remember_token])
-					log_in user
-					@current_user = user
-				end
-			end		
-	end
+  #Returns the user corresponding to the remember token choice
+  def current_user
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+      elsif (user_id = cookies.signed[:user_id])
+        user = User.find_by(id: user_id)
+        if user && user.authenticated?(cookies[:remember_token])
+          log_in user
+          @current_user = user
+        end
+      end   
+  end
 
-	#Returns True if the User is Logged in, false otherwies
-	def loggged_in?
-		!current_user.nil?
-	end
+  #Returns True if the User is Logged in, false otherwies
+  def loggged_in?
+    !current_user.nil?
+  end
 
-	#Forgets a persistent session
-	def forget(user)
-		user.forget
-		cookies.delete(:user_id)
-		cookies.delete(:remember_token)
-	end
+  #Forgets a persistent session
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
 
-	#Logs out the current user
-	def log_out
-		forget(current_user)
-		session.delete(:user_id)
-		@current_user = nil
-	end
+  #Logs out the current user
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
 end
